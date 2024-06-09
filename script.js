e13d738e3ff0e49cb8f2985b3dc0721cd8524d02
Sixto Arrejin <arrejinsixto@gmail.com>
2024-06-09 05:26:27 -0300

    problema de importación 'solucionado'

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-export default function sumar() {
+function sumar() {
     var num1 = document.getElementById('num1').value;
     var num2 = document.getElementById('num2').value;
     num1 = parseFloat(num1);
