4be2e70beb7ddb6fafa415907793579553fa224d
Sixto Arrejin <arrejinsixto@gmail.com>
2024-06-09 00:37:15 -0300

    fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,47 @@
 import jsConfig from '@eslint/js';
 
-export default [
-  jsConfig.configs.recommended,
-  {
-    files: ['**/*.js'],
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
-      globals: {
-        browser: true,
-        es2021: true,
-        document: 'readonly',  // Añadir la global 'document'
-      }
-    },
-    rules: {
-      'no-unused-vars': 'warn',
-      'no-console': 'off'
+// Configuración principal de ESLint
+const mainConfig = {
+  files: ['**/*.js'],
+  languageOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    globals: {
+      browser: true,
+      es2021: true,
+      document: 'readonly',  // Añadir la global 'document'
     }
   },
-  {
-    files: ['test/test.js'], // Ajusta la ruta a tus archivos de prueba
-    languageOptions: {
-      globals: {
-        describe: 'readonly',
-        it: 'readonly',
-        expect: 'readonly'
-      }
-    },
-   
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'off'
   }
+};
+
+// Configuración para archivos de prueba
+const testConfig = {
+  files: ['test/test.js'], // Ajusta la ruta a tus archivos de prueba
+  languageOptions: {
+    globals: {
+      describe: 'readonly',
+      it: 'readonly',
+      expect: 'readonly'
+    }
+  }
+};
+
+// Configuración para ignorar la carpeta cypress
+const ignoreCypressConfig = {
+  files: ['cypress/**/*'],
+  rules: {
+    'no-unused-vars': 'off',
+    'no-console': 'off'
+  }
+};
+
+// Exportar la configuración combinada
+export default [
+  mainConfig,
+  testConfig,
+  ignoreCypressConfig
 ];
