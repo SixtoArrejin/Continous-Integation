f8af3a5f4410e686c3c69d628c73b18dbbd195e7
Sixto Arrejin <arrejinsixto@gmail.com>
2024-06-12 02:54:18 -0300

    Introduciendo un 4to code smell

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 function sumar() {
   var num1 = document.getElementById('num1').value;
   var num2 = document.getElementById('num2').value;
-  var result = sum(num1, num2);
+  var cero = 0;
+  var result = sum(num1, num2+cero);
   document.getElementById('resultado').innerText = result;
 }
 
